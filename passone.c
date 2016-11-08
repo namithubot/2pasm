56f2e3ea86f46b559967cedf3c947814ddb59f4e
SHUBHAM JAIN <shubham96jain@gmail.com>
2016-11-09 03:19:34 +0530

    Cleaner version of passone.c

--- passone.c
+++ passone.c
@@ -4,47 +4,42 @@
 #include"sicAssembler.c"
 void main()
 {
-	FILE * fpsource,fpinter;
-	char line[128],label[45],opcode[45],operand[45];
+	FILE * fpsource,*fpinter;
+	char line[128],label[45],opcode[45],operand[45],operand_value[10];
 	long int locctr,program_length;
 	bool error_flag = false;
 	struct symtab *first = NULL;
-	int i=0,count=0,operand_value[10],starting_add;
+	int i=0,count=0,starting_add;
 //opeing source code file
 	fpsource= fopen("source.2pasm","r");
-	if (fp==NULL)
+	if (fpsource==NULL)
 	{
 		printf("\nError opening the file.");
 	}
 //reading first input line
-	line = fgets(line,128,fpsource);
+	fgets(line,128,fpsource);
 //getting values of label,opcode,operand
-	label = get_label(line);
-	opcode = get_op_code(line,label);
-	operand = get_operand(line,label,opcode);
+	strcpy(label,get_label(line));
+	strcpy(opcode,get_op_code(line,label));
+	strcpy(operand,get_operand(line,label,opcode));
 //if the opcode is START
 	if(strcmp(opcode,"START")==0)	
 	{
-		starting_add = operand;
+		sscanf(operand,"%d",&starting_add);
 //initializing location counter with the value of operand of start
-		locctr = operand;		
+		sscanf(operand,"%ld",&locctr);		
 		fpinter = fopen("source.2pasm.temp","w");
 //writing line in the intermediate file
-		if(fputs(line,fpinter);)
+		if(fputs(line,fpinter))
 			printf("\nError writing in intermediate file.");
 //reading next input line
-		line = read_next_inputline(fpsource);	
+		strcpy(line,read_next_input_line(fpsource));	
 	}
 	else
 		locctr = 0;
 	while(strcmp(opcode,"END")==0)
 	{
 		if(line[0]=='.')
-		{
-			line = read_next_inputline(fpsource);
-			break;
-		}
-		else
 		{
 			if(label!=NULL)
 			{
@@ -72,12 +67,12 @@ void main()
 			}
 			else if(strcmp(opcode,"RESW"))
 			{
-				locctr += 3*operand;
+				locctr += 3*atoi(operand);
 				break;
 			}
 			else if(strcmp(opcode,"RESB"))
 			{
-				locctr += operand;
+				locctr += atoi(operand);
 				break;
 			}
 			else if(strcmp(opcode,"BYTE"))
@@ -97,7 +92,7 @@ void main()
 				{
 					while(operand[i]!='\0')
 					{
-						if(i>1&&(operand[i]!='''))
+						if(i>1&&(operand[i]!='\''))
 						{
 							operand_value[i-2] = operand[i];
 						}
@@ -114,7 +109,7 @@ void main()
 			}
 		}
 		write_line_to_intermediate_file(fpinter,line);
-		line = read_next_input_line(fpsource)
+		strcpy(line,read_next_input_line(fpsource));
 	}	
 	write_line_to_intermediate_file(fpinter,line);
 	program_length = locctr - starting_add;
