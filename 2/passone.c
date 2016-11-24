4e05a7a141502173e89c95cbbbbac74ad0a112ae
namithubot <namithubot@gmail.com>
2016-11-24 14:25:37 +0530

    Minor changes in 2

--- 2/passone.c
+++ 2/passone.c
@@ -2,13 +2,13 @@
 #include<string.h>
 void main()
 {
-  FILE *f1,*f2,*f3, *f4;
+  FILE *ip,*optab,*sym, *lenfile;
 int lc,sa,l,op1,o,len;
 char m1[20],la[20],op[20],otp[20];
- f1=fopen("input.2pasm","r");
- f3=fopen("symtab","w");
- f4=fopen("length", "w");
-fscanf(f1,"%s %s %d",la,m1,&op1);
+ ip=fopen("input.2pasm","r");
+ sym=fopen("symtab","w");
+ lenfile=fopen("length", "w");
+fscanf(ip,"%s %s %d",la,m1,&op1);
 if(strcmp(m1,"START")==0)
  {
 sa=op1;
@@ -17,27 +17,27 @@ printf("\t%s\t%s\t%d\n",la,m1,op1);
  }
 else
 lc=0;
-fscanf(f1,"%s %s",la,m1); 
-while(!feof(f1))
+fscanf(ip,"%s %s",la,m1); 
+while(!feof(ip))
  {
-fscanf(f1,"%s",op);
+fscanf(ip,"%s",op);
 printf("\n%d\t%s\t%s\t%s\n",lc,la,m1,op);
  if(strcmp(la,"-")!=0)
   {
-fprintf(f3,"\n%d\t%s\n",lc,la);
+fprintf(sym,"\n%d\t%s\n",lc,la);
   }
-  f2=fopen("optab","r");
-fscanf(f2,"%s %d",otp,&o);
-while(!feof(f2))
+  optab=fopen("optab","r");
+fscanf(optab,"%s %d",otp,&o);
+while(!feof(optab))
   {
 if(strcmp(m1,otp)==0)
    {
 lc=lc+3;
 break;
    }
-fscanf(f2,"%s %d",otp,&o);
+fscanf(optab,"%s %d",otp,&o);
  }
-fclose(f2);
+fclose(optab);
 if(strcmp(m1,"WORD")==0)
 {
 lc=lc+3;
@@ -62,14 +62,14 @@ else if(strcmp(m1,"RESB")==0)
 op1=atoi(op);
 lc=lc+op1;
 }
-fscanf(f1,"%s%s",la,m1);
+fscanf(ip,"%s%s",la,m1);
  }
 if(strcmp(m1,"END")==0)
  {
 printf("Program length =%d\n",lc-sa);
- fprintf(f4, "%d", lc-sa);
+ fprintf(lenfile, "%d", lc-sa);
 }
-fclose(f1);
-fclose(f3);
- fclose(f4);
+fclose(ip);
+fclose(sym);
+ fclose(lenfile);
 }
